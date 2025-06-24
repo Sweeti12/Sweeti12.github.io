acbc1d5cb02f780d62c09773a4659945ee79723e
Sweeti12 <sweetihivarkar12@gmail.com>
2025-06-24 15:04:28 +0530

    feat: Enable CORS on backend server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Enable CORS
+app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(express.json());
